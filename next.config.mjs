97d8b1fa2dea7f371285858174c27a56f07cd8bf
王浩羽 <2453578@tongji.edu.cn>
2026-05-29 17:45:00 +0800

    3

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isUserOrOrgSite = repo?.endsWith('.github.io')
+const basePath = isGithubActions && repo && !isUserOrOrgSite ? `/${repo}` : ''
+
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath,
+  assetPrefix: basePath,
   typescript: {
     ignoreBuildErrors: true,
   },
